a62c92f5877040e4c721f78caa9f6f2b252bd76e
Promiseigboke <igbokesomtochukwupromise@gmail.com>
2023-02-06 06:05:14 +0100

    I reduced the currentButton.style.transform

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function handleMouseOver(arabic) {
     let currentButton = document.getElementById(arabic)
-    currentButton.style.transform = 'scale(1.3)'
+    currentButton.style.transform = 'scale(1.1)'
     currentButton.style.zIndex = '9'
     currentButton.textContent = arabic
 }
 
 function handleMouseOut (id, roman) {
     let currentButton = document.getElementById(id)
-    currentButton.style.transform = 'scale(1)'
+    currentButton.style.transform = 'scale(0.9)'
     currentButton.stylezIndex ='0'
     currentButton.textContent = roman
 }
